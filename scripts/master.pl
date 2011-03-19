08e0767b37b79d11b8d9e04792d97f64631dc0a7
chen <chengchen09@gmail.com>
2011-03-19 05:46:45 -0700

    master

--- scripts/master.pl
+++ scripts/master.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-use Getopt::Long;
+use Getopt::Long qw(:config gnu_getopt);
 
 my $progname = "master";
 
@@ -11,20 +11,39 @@ main();
 # --hdf52nc4 means hdf5 transform to nc4
 sub main {
 	
-	my $nprocs;
+	my $help = -1;	# if 1, print help information
+	my $ph52nc4 = -1;	# if 1, launch ph52nc4
+
+	my $nprocs;	# number of processes should be lauched
+	my $status;
 
 	if (!(defined @ARGV)) {
 		usage();
-	}
-	else {
-		printf("%d\n", $#ARGV+1);
+		exit(0);
 	}
 
-	$nprocs = get_nprocs();
+	GetOptions("help|h|H" => \$help,
+			   "ph52nc4" => \$ph52nc4);
+
+#	printf("%d\n", $#ARGV+1);
+#	print("$ARGV[0]\n");
+	if (1 == $help) {
+		usage();
+		exit(0);
+	}
+
+	if (1 == $ph52nc4) {
+		foreach (@ARGV) {
+			$nprocs = get_nprocs($_);
+			print("$_\n");
+		}
+		exit(0);
+	}
 }
 
 sub usage {
-	print("Usage: $progname [OPTION] ... inputfile [outputfile]\n");
+	print("Usage: $progname [OPTION] ... inputfile [outputfile]\n".
+	"Example: $progname --ph52nc4 *\n");
 }
 
 
