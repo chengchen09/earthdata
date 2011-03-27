dd63ec1307f5d5775bdceb614ca141890f2d327a
chen <chengchen09@gmail.com>
2011-03-26 21:08:50 -0700

    master.py

--- scripts/master.py
+++ scripts/master.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import getopt
 
+def manager():
+#	print len(sys.argv)
+#	for arg in sys.argv:
+#		print arg
+	
+
+def usage():
+	"""usage: progname [OPTION] ... inputfile
+	Example: progname --ph52nc4"""
+
+#print usage.__doc__
+
+if __name__ == '__main__':
+	manager()
