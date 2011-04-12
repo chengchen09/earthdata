9a25b076e41df9fea607139435ba972531367f47
chen-vm <chen@ubuntu.(none)>
2011-04-12 20:29:02 +0800

    index

--- nc32h5/index.py
+++ nc32h5/index.py
@@ -5,39 +5,66 @@ import sys
 import rtree
 import h5py
 
-def create_index(h5path, variable, dimension):
+def create_index(h5path, variable, dimensions):
 	h5_fh = h5py.File(h5path, 'r')
+	var_id = h5_fh[variable]
+	dim_ids = []
+	for dim in dimensions:
+		dim_id = h5_fh[dim]
+		dim_ids.append(dim_id)
 	
-	ndims = len(dimension)
-	ds = h5_fh[variable]
+	chunk_objs = []
+	create_2D_objs(var_id, dim_ids, chunk_objs)
 	
-	dim_hs = []
-	for path in dimension:
-		dim_h = h5_fh[path]
-		dim_hs.append(dim_h)
+	# create index
+	p = rtree.index.Property()
+	idx = rtree.index.Index(properties=p)
+	for chunk_obj in chunk_objs:
+		#print chunk_obj['id'], chunk_obj['coordinates'], chunk_obj['obj']
+		idx.insert(chunk_obj['id'], chunk_obj['coordinates'], chunk_obj['obj'])	
+
+	h5_fh.close()
+	return idx
+
+
+def create_2D_objs(var_id, dim_ids, chunk_objs):
+	if var_id.chunks == None:
+		print "variable is not chunked"
+		sys.exit(1)
+		
+	chunk_dims = list(var_id.chunks)
+	var_dims = list(var_id.shape)
+	x = dim_ids[0]
+	y = dim_ids[1]
 	
+	chunk_nums = []
+	nchunks = 1
+	for i in range(2):
+		n = (var_dims[i] - 1) / chunk_dims[i] + 1
+		chunk_nums.append(n)
+		nchunks *= n
+
+	for i in range(chunk_nums[0]):
+		xmin = x[i * chunk_dims[0]]
+		xmax = x[min(var_dims[0] - 1, (i + 1) * chunk_dims[0] - 1)]
+		for j in range(chunk_nums[1]):
+			ymin = y[j * chunk_dims[1]]
+			ymax = y[min(var_dims[1] - 1, (j + 1) * chunk_dims[1] - 1)]
+			chunk_obj = {}
+			chunk_obj['id'] = i * chunk_nums[1] + j
+			chunk_obj['coordinates'] = (xmin, ymin, xmax, ymax)
+			chunk_obj['obj'] = str(i) + ':' + str(j)
+			#print chunk_obj['id'], chunk_obj['coordinates'], chunk_obj['obj']
+			#data = raw_input("")	
+			chunk_objs.append(chunk_obj)
+
+def test_create():
+	dims = ['/y', '/x']
+	idx = create_index(sys.argv[1], '/z', dims)
 	
-	
-
-def insert():
-	pass
-
-
-def delete():
-	pass
-
-
-def search():
-	pass
-
-
-def load():
-	pass
-
-
-def test():
-	pass
-
+	hits = list(idx.intersection((0, 0, 60, 60), objects=True))
+	for item in hits:
+		print item.id, item.bbox, item.object
 
 if __name__ == '__main__':
-	test()
+	test_create()
